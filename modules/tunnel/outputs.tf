422db116bdb103da6d929df0f7bda2e731420057
jhrr <jhrr@users.noreply.github.com>
2025-01-26 14:41:14 +0000

    Remove obsolete 'tunnel' module

--- modules/tunnel/outputs.tf
+++
@@ -1,6 +0,0 @@
-# Outputs for the AWS SSM Tunnel Terraform module.
-
-output "security_group_id" {
-  description = "Identifier for the tunnel's security group."
-  value       = aws_security_group.jump_host.id
-}
